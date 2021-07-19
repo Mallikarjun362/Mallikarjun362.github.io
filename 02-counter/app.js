0b9b0243ce0f5205335a576665d91d9d2b781f8b
Mallikarjun362 <54075404+Mallikarjun362@users.noreply.github.com>
2021-07-19 07:42:12 +0530

    Add files via upload

---
+++ 02-counter/app.js
@@ -0,0 +1,31 @@
+let count = 0;
+
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+
+
+
+btns.forEach(function(btn){
+
+    btn.addEventListener('click',function(e){
+
+        if ("increase" === e.currentTarget.classList[1]){
+            count += 1;
+        }else if ("decrease" === e.currentTarget.classList[1]){
+            count -= 1;
+        }else if ("reset" === e.currentTarget.classList[1]){
+            count = 0;
+            value.style.color = 'grey';
+        }
+        value.innerHTML = count;
+        if(count > 0){
+            value.style.color = "green";
+        }else if(count <0){
+            value.style.color = "red";
+        }
+    });
+
+
+});
